71526752dd5c8f456e3b6239a55ae7b3c1ce1028
Pvblo <pmonjes@ing.ucsc.cl>
2024-04-25 22:14:32 -0400

    pruebas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,13 @@
 
 <head>
     <meta charset="utf-8">
-    <title>KOPPEE - Coffee Shop HTML Template</title>
+    <title>Cerveza Fuzz</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free Website Template" name="keywords">
     <meta content="Free Website Template" name="description">
 
     <!-- Favicon -->
-    <link href="img/beer.png" rel="icon">
+    <link href="img/fuzz/logo4.png" rel="icon">
 
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -35,7 +35,7 @@
     <div class="container-fluid p-0 nav-bar">
         <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
             <a href="index.html" class="navbar-brand px-lg-4 m-0">
-                <h1 class="m-0 display-4 text-uppercase text-white">KOPPEE</h1>
+                <h1 class="m-0 display-5 text-uppercase text-white">Cerveza Fuzz</h1>
             </a>
             <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
@@ -65,19 +65,19 @@
         <div id="blog-carousel" class="carousel slide overlay-bottom" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="img/carousel-1.jpg" alt="Image">
+                    <img class="w-100" src="img/fuzz/etiquetas/devil.png" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                        <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
-                        <h1 class="display-1 text-white m-0">COFFEE</h1>
-                        <h2 class="text-white m-0">* SINCE 1950 *</h2>
+                        
+                        <h3 class="display-1 text-white m-0">Fuzz</h3>
+                        
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img class="w-100" src="img/carousel-2.jpg" alt="Image">
+                    <img class="w-100" src="img/fuzz/etiquetas/elrucio.png" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                        <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
-                        <h1 class="display-1 text-white m-0">BEER</h1>
-                        <h2 class="text-white m-0">* SINCE 1950 *</h2>
+
+                        <h3 class="display-1 text-white m-0">Fuzz</h3>
+
                     </div>
                 </div>
             </div>
