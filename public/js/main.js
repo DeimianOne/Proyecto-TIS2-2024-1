1b36974bfe93bea947d4e3d6046532ed4c41f385
Damián Pantoja <62729878+DeimianOne@users.noreply.github.com>
2024-05-07 23:51:10 -0400

    Revert "Merge branch 'pablo-monjes' into damian-pantoja"
    This reverts commit f2ec87d3bd20eeb643ec7b4e8ef80bef538e1191, reversing
    changes made to c981cf8b12754eef4115bd3651d7a0a78f50734e.

--- public/js/main.js
+++ public/js/main.js
@@ -19,26 +19,6 @@
     });
 
 
-    $(document).ready(function() {
-        var $ageVerificationPopup = $("#ageVerificationPopup");
-        var $yesButton = $("#yesButton");
-        
-        // Verificar si ya se ha verificado la edad
-        var ageVerified = localStorage.getItem("ageVerified");
-        if (ageVerified) {
-            // Si se ha verificado la edad, ocultar el popup
-            $ageVerificationPopup.hide();
-        }
-    
-        $yesButton.on("click", function() {
-            // Al hacer clic en "Sí", ocultar el popup y guardar en el almacenamiento local
-            $ageVerificationPopup.hide();
-            localStorage.setItem("ageVerified", true);
-        });
-    });
-    
-    
-
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -74,31 +54,28 @@
 
 
     // Testimonials carousel
-    // $(".testimonial-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1500,
-    //     margin: 30,
-    //     dots: true,
-    //     loop: true,
-    //     center: true,
-    //     responsive: {
-    //         0:{
-    //             items:1
-    //         },
-    //         576:{
-    //             items:1
-    //         },
-    //         768:{
-    //             items:2
-    //         },
-    //         992:{
-    //             items:3
-    //         }
-    //     }
-    // });
-
-
-    
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
 
 })(jQuery);
 
--- routes/web.php
+++ routes/web.php
@@ -15,50 +15,6 @@ use App\Http\Controllers\PostController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
-
-Route::get('/packs', function () {
-    return view('packs');
-})->name('packs');
-
-Route::get('/iniciarsesion', function () {
-    return view('iniciarsesion');
-})->name('iniciarsesion');
-
-Route::get('/registrate', function () {
-    return view('registrate');
-})->name('registrate');
-
-Route::get('/cervezas', function () {
-    return view('cervezas');
-})->name('cervezas');
-
-Route::get('/armatupack', function () {
-    return view('armatupack');
-})->name('armatupack');
-
-Route::get('/product-pack6', function () {
-    return view('product-pack6');
-})->name('product-pack6');
-
-Route::get('/product-pack12', function () {
-    return view('product-pack12');
-})->name('product-pack12');
-
-Route::get('/contacto', function () {
-    return view('contacto');
-})->name('contacto');
-
-Route::get('/nosotros', function () {
-    return view('nosotros');
-})->name('nosotros');
-
-Route::get('/dondeestamos', function () {
-    return view('dondeestamos');
-})->name('dondeestamos');
-
 // returns the home page with all posts
 // Route::get('/', PostController::class .'@index')->name('posts.index');
 // returns the form for adding a post
@@ -85,4 +41,3 @@ Route::match(['get', 'post'], '/dashboard', function(){
 Route::view('/pages/slick', 'pages.slick');
 Route::view('/pages/datatables', 'pages.datatables');
 Route::view('/pages/blank', 'pages.blank');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
