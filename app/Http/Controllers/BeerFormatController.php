e5e70f729c812d098253fe7d2dce43422503518d
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2024-05-13 22:55:57 -0400

    update controller beer format
    aun sin funcionar el creado de datos de formato cerveza

--- app/Http/Controllers/BeerFormatController.php
+++ app/Http/Controllers/BeerFormatController.php
@@ -35,7 +35,7 @@ class BeerFormatController extends Controller
             'liters' => $request->input('liters'),
         ]);
 
-        return redirect()->route('format.index')->with('mensaje','Formato de cerveza agregado con éxito');
+        return redirect()->route('beerFormat.index')->with('mensaje','Formato de cerveza agregado con éxito');
 
         //return redirect('beerFormat')->with('mensaje','Formato cerveza agregado con éxito');
     }
