7e9a4c2530ef50828d71feddf96d1ba4bd72c34b
Damián Pantoja <62729878+DeimianOne@users.noreply.github.com>
2024-05-24 20:52:51 -0400

    Reestructuración Base de Datos (eliminación archivos actuales)

--- resources/views/product/edit.blade.php
+++
@@ -1,9 +0,0 @@
-Sección para editar producto
-
-<form action="{{ url('/product/'.$product->id) }}" method="post">
-@csrf
-{{ method_field('PATCH') }}
-@include('product.form', ['modo'=>'Editar'] )
-
-</form>
-
