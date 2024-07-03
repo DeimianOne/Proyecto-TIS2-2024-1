37899eec1fc435b80737034a72447a865e26f082
Damián Pantoja <62729878+DeimianOne@users.noreply.github.com>
2024-07-03 03:22:22 -0400

    Update 2024_05_16_164935_create_beers_table.php

--- database/migrations/2024_05_16_164935_create_beers_table.php
+++ database/migrations/2024_05_16_164935_create_beers_table.php
@@ -17,7 +17,6 @@ class CreateBeersTable extends Migration
             $table->id();
             $table->foreignId('product_id')->constrained('products');
             $table->foreignId('beerstyle_id')->constrained('beerstyles');
-            $table->string('container')->default('default_container');
             $table->integer('liter_value');
             $table->timestamps();
         });
